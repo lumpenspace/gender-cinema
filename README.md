1e73b86528ef918e96573601229c24ba6e7475fc
Lumpenspace <lumpenspace@gmail.com>
2023-12-19 14:30:23 +0100

    Parse data and set scores

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gender-cinema
 is it true that men like "random silly shit is happening" humor and women do not? lets investigate
+
+## Data
+
+The movies and relative rankings data is originally from imdb; i scraped it from [fivethirtyeight](https://fivethirtyeight.com/features/what-if-online-movie-ratings-werent-based-almost-entirely-on-what-men-think/?ex_cid=538twitter).
+
+The bechdel data is courtesy of fivethirtyeight again: [bechdel test dataset](https://github.com/fivethirtyeight/data/tree/master/bechdel).
+
---
+++ rankings.py
@@ -0,0 +1,57 @@
+import json
+import pandas as pd
+
+def open_path(name, mode, type="json"):
+    return open(f"data/{name}.{type}", mode)
+
+def save_json_file(name, data):
+    with open_path(name, 'w') as file:
+        json.dump(data, file)
+
+def open_json_file(name):
+    with open_path(name, 'r') as file:
+        json_input = json.load(file)
+    return json_input
+
+def normalize_rank(rank):
+    """ Normalize the rank to a scale from 0 to 1 """
+    return 1 - (int(rank) - 1) / 499.0
+
+def normalize_rank(rank):
+    """ Normalize the rank to a scale from 0 to 1 """
+    return 1 - (rank - 1) / 499.0
+
+def calculate_gender_score(rank_a, rank_b):
+    """ Calculate the weighted score based on normalized ranks and average rank """
+    normalized_a = normalize_rank(rank_a)
+    normalized_b = normalize_rank(rank_b)
+
+
+    return (normalized_b - normalized_a) 
+
+def parse_movies_with_gender_score(input_list):    
+
+    parsed_data = []
+    for item in input_list:
+        split_item = item.split()
+        year = split_item[0]
+        ranking, ranking_women, ranking_men = [int(ranking) for ranking in split_item[-4:-1]]
+        title = ' '.join(split_item[1:-4])
+        gender_score = calculate_gender_score(ranking_women, ranking_men)
+
+        parsed_data.append([year, title, ranking, ranking_women, ranking_men, gender_score])
+
+    df = pd.DataFrame(parsed_data, columns= ['year', 'title', 'ranking', 'ranking_women', 'ranking_men', 'gender_score'])
+    return df
+
+
+def main():
+    json_input = open_json_file('preferences_table')
+    parsed = parse_movies_with_gender_score(json_input)
+    print(parsed.head(39))
+
+    saved = save_json_file('parsed_pref_table', parsed.to_dict('records'))
+    print(saved)
+
+if __name__ == "__main__":
+    main()
